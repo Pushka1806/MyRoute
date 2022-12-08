aba78a72b68442a4235895da3a2de68e95c6572f
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2022-12-08 14:08:08 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const controller = require('../controllers/auth')
 const router = express.Router()
 
-router.post('/addDriver', controller.addDriver)
+router.get('/addDriver', controller.addDriver)
 
 // вход в систему по паролю
 router.post('/login', controller.login)      // y
