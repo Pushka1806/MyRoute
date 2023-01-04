a956ec7ebaff3dbafc32d5c43a3142e6390df445
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2023-01-04 23:54:30 +0300

    Update busStops.js

--- controllers/busStops.js
+++ controllers/busStops.js
@@ -1,7 +1,33 @@
+const Driver_route = require('../models/Driver_routes')
+
+
+
 module.exports.getAllById = function (req, res){
    res.status(201).json("OK")
 }
 
 module.exports.getStopsByRouteStart = function (req, res){
-   res.status(201).json("OK")
+   try {
+        const route = (await Driver_route.findOne({ "_id": req.query.route }))
+        let startPoint = false
+        let availableStops = new Array()
+        for (let stop of route.route) {
+            while (startPoint != true) {
+                if (stop === req.query.start) {
+                    startPoint = true
+                }
+                else {
+                    continue
+                }
+            }
+            availableStops.push(stop)
+        }
+        res.status(201).json(availableStops)
+    } catch (e) {
+        res.status(501).json({      // ошибки в серверной части
+            message: "Ошибка сервера. Попробуйте снова"
+        })
+        console.log(e)
+    }
+
 }
