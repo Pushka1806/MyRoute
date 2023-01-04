43ef3aa6dab9ac65691078987059755b0bcd533c
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2023-01-04 23:48:01 +0300

    Update busStops.js

--- routes/busStops.js
+++ routes/busStops.js
@@ -5,4 +5,6 @@ const router = express.Router();
 //localhost:5000/api/busStops/login
 router.get('/:id', controller.getAllById);
 
+router.get('/getStopsByRouteStart', controller.getStopsByRouteStart);
+
 module.exports = router;
