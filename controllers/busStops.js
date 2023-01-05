56c3b4519fdbbdcf11a09c9d12a65fab6fad2b2b
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2023-01-05 17:17:10 +0300

    yui

--- controllers/busStops.js
+++ controllers/busStops.js
@@ -1,6 +1,20 @@
 const routes = require('../models/Driver_routes')
 
 module.exports.getStopsByRouteStart =  async function (req, res) {
-    const availableStops = await routes.findOne({ "_id": req.query.route })
+    const allStops = await routes.findOne({ "_id": req.query.route })
+    let startPoint = false
+    let availableStops = new Array()
+    for (let stop of allStops.route) {
+        while (startPoint != true) {
+            if (stop === req.query.start) {
+                startPoint = true
+            }
+            else {
+                continue
+            }
+        }
+        availableStops.push(stop)
+    }
     res.status(201).json(availableStops)
+    
 }
