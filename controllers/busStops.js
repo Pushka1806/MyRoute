8257b8d73ff12fe28868f07f30a6a7882b899cb4
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2023-01-05 15:38:01 +0300

    45 123

--- controllers/busStops.js
+++ controllers/busStops.js
@@ -10,20 +10,20 @@ module.exports.getStopsByRouteStart = function (req, res) {
     console.log("aboba")
     try {
         //const route = (await route_model.findOne({ "_id": req.query.route }))
-        let startPoint = false
-        let availableStops = new Array()
-        for (let stop of route.route) {
-            while (startPoint != true) {
-                if (stop === req.query.start) {
-                    startPoint = true
-                }
-                else {
-                    continue
-                }
-            }
-            availableStops.push(stop)
-        }
-        res.status(201).json(availableStops)
+        //let startPoint = false
+        //let availableStops = new Array()
+        //for (let stop of route.route) {
+        //    while (startPoint != true) {
+        //        if (stop === req.query.start) {
+        //            startPoint = true
+        //        }
+        //        else {
+        //            continue
+        //        }
+        //    }
+        //    availableStops.push(stop)
+        //}
+        //res.status(201).json(availableStops)
     } catch (e) {
         res.status(501).json({      // ошибки в серверной части
             message: "Ошибка сервера. Попробуйте снова"
