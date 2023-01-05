b9eb702189388d3256040d6f98a4ea8246ef55b0
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2023-01-05 16:14:24 +0300

    async

--- controllers/busStops.js
+++ controllers/busStops.js
@@ -1,6 +1,6 @@
 const routes = require('../models/Driver_routes')
 
-module.exports.getStopsByRouteStart = function (req, res) {
+module.exports.getStopsByRouteStart =  async function (req, res) {
     const availableStops = await routes.findOne({ "_id": req.query.route })
     res.status(201).json(availableStops)
 }
