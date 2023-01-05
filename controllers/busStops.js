bc76046d26aa71bdc0ebf4b56dff4de555268d4d
Pushka1806 <75617619+Pushka1806@users.noreply.github.com>
2023-01-05 15:50:52 +0300

    йц

--- controllers/busStops.js
+++ controllers/busStops.js
@@ -1,5 +1,3 @@
-const route_model = require('../models/Driver_routes')
-
 
 module.exports.getStopsByRouteStart = function (req, res) {
     res.status(201).json("aboba")
--- routes/busStops.js
+++ routes/busStops.js
@@ -1,10 +1,7 @@
-const express = require('express');
-const controller = require('../controllers/busStops');
-const router = express.Router();
+const express = require('express')
+const controller = require('../controllers/busStops')
+const router = express.Router()
 
-//localhost:5000/api/busStops/login
-router.get('/:id', controller.getAllById);
+router.get('/getStopsByRouteStart', controller.getStopsByRouteStart)
 
-router.get('/getStopsByRouteStart', controller.getStopsByRouteStart);
-
-module.exports = router;
+module.exports = router
